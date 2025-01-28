14ba4442184da01d5f17b35120a8dc70c9207b9d
PyskoMaxym <pisko.maks@gmail.com>
2025-01-28 14:54:36 +0200

    update main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import  store  from "./redux/store.js"
 import './index.css'
 import App from './App.jsx'
@@ -8,7 +9,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
     <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
