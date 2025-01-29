e52e6e247c6c32d389a2d588797ae5d650f1a971
PyskoMaxym <pisko.maks@gmail.com>
2025-01-29 12:33:44 +0200

    add persistor import in main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import  store  from "./redux/store.js"
+import  store, {persistor}  from "./redux/store.js"
 import './index.css'
 import App from './App.jsx'
 
